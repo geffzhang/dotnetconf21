c87c6138e55bd2fa67a6658cced26c9448fdf79c
Léger Djiba <leger.djiba@outlook.fr>
2021-11-18 17:21:33 +0000

    Many others modifications

---
+++ DotNetConf21.WinForm/Form1.cs
@@ -0,0 +1,15 @@
+namespace DotNetConf21.WinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Hello World from .Net Conf 2021 : West Africa and Arabic";
+        }
+    }
+}
