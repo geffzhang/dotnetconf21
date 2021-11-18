ca014fcffb0b4372633a9cc07157b00dd0747544
Léger Djiba <leger.djiba@outlook.fr>
2021-11-18 17:12:24 +0000

    first commit

---
+++ MauiApp7/obj/Debug/net6.0-android/android/src/crc6406c3b64641181843/MainActivity.java
@@ -0,0 +1,46 @@
+package crc6406c3b64641181843;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("MauiApp7.MainActivity, DotNetConf21.MAUI2", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("MauiApp7.MainActivity, DotNetConf21.MAUI2", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("MauiApp7.MainActivity, DotNetConf21.MAUI2", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ MauiApp7/obj/Debug/net6.0-android/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,12 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("MauiApp7.MainApplication, DotNetConf21.MAUI2, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6406c3b64641181843.MainApplication.class, crc6406c3b64641181843.MainApplication.__md_methods);
+		mono.android.Runtime.register ("Microsoft.Maui.MauiApplication, Microsoft.Maui, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6488302ad6e9e4df1a.MauiApplication.class, crc6488302ad6e9e4df1a.MauiApplication.__md_methods);
+		
+	}
+}
