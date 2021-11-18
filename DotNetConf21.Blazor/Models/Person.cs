4ac89f2c770a0fcdee89b376cec01499f4c3fa0e
Léger Djiba <leger.djiba@outlook.fr>
2021-11-18 19:49:01 +0000

    Reload New Maui Project

---
+++ DotNetConf21.Blazor/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace DotNetConf21.Blazor.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string? Adress { get; set; }
+        public float Amount { get; set; }
+    }
+}
---
+++ DotNetConf21.MAUI2/obj/Debug/net6.0-android/android/src/crc6416946d78e98fbf16/MainActivity.java
@@ -0,0 +1,46 @@
+package crc6416946d78e98fbf16;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("DotNetConf21.MAUI2.MainActivity, DotNetConf21.MAUI2", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("DotNetConf21.MAUI2.MainActivity, DotNetConf21.MAUI2", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("DotNetConf21.MAUI2.MainActivity, DotNetConf21.MAUI2", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
